5c19940edc54862cb8ec3e5566c0dbafa769e51a
keshavgera1 <keshav.gera@enfec.com>
2024-01-08 17:55:44 +0530

    versel host node app

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 4500;
+
+app.listen(PORT, () =>{
+   console.log(`API listening on port ${PORT}`);
+});
+
+app.get('/', (req, res) =>{
+   res.send('Hey this is my api running....... on port 4500 ');
+});
+
+module.export = app;
+
+
